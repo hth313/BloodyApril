6ee7690d8d47f3ef496e1f4899b6e88aba532bf0
Håkan Thörngren <hth313@gmail.com>
2023-07-10 14:29:40 -0700

    Add aerodrome type
    This is an airfield that hosts one or several squadrons. It also
    collects airplanes that are on the ground. For now such airplanes can
    either be loose or connected in flights.

---
+++ src/aerodrome.h
@@ -0,0 +1,23 @@
+#ifndef __AERODROME_H__
+#define __AERODROME_H__
+
+#include "coordinate.h"
+#include "list.h"
+#include "location.h"
+
+// An aerodrome is a place where airplanes are stored, takes off from
+// and can land at. They have a fixed location and identity in the map
+// when they are active.
+// Airplanes on the ground are either kept in a flights (group of airplanes)
+// plus a flat list of airplanes that may be added to a flight.
+typedef struct aerodrome {
+  struct node node;           // Belongs to a list of existing aerodromes
+  const char *name;           // Name of the aerodrome
+  coordinate pos;             // The sector of this aerodrome
+  bool allied;                // Set if this aerodrom belongs to the allied side.
+  struct list flights;        // All the flights parked here.
+  struct list airplanes;      // Airplanes on the ground not currently part of a flight
+  struct list squadron;       // The squadrons stationed at this aerodrome
+} aerodrome;
+
+#endif // __AERODROME_H__
