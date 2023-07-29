c1875d52ce44aa6470e7a61916f1f573dfc5c7f1
Håkan Thörngren <hth313@gmail.com>
2023-07-29 11:24:55 -0600

    Add Germen aerodromes
    Given the French names use ISO Latin-1 encoding for the file.

--- src/aerodrome.c
+++ src/aerodrome.c
@@ -1,3 +1,4 @@
+// -*- coding: iso-latin-1 -*-
 #include "aerodrome.h"
 #include "actor_sprite.h"
 #include "coordinate.h"
@@ -9,6 +10,24 @@ extern uint8_t central_aerodrome_sprite_data[SPRITE_SIZE];
 
 struct aerodrome Provin;
 struct aerodrome Proville;
+struct aerodrome Pronville;
+struct aerodrome Guesnain;
+struct aerodrome Phalempin;
+struct aerodrome Faumont;
+struct aerodrome Bersee;
+struct aerodrome Roucourt;
+struct aerodrome Epinoy;
+struct aerodrome Villers_au_Tetre;
+struct aerodrome LaBrayelle;
+struct aerodrome Gonnelieu;
+struct aerodrome Boistrancourt;
+struct aerodrome Eswars;
+struct aerodrome Pont_a_Marcq;
+struct aerodrome Cuincy;
+struct aerodrome Cantin;
+struct aerodrome Emerchicourt;
+struct aerodrome Corbehem;
+struct aerodrome Abacon;
 
 struct sprite allied_aerodrome;
 struct sprite central_aerodrome;
@@ -41,4 +60,22 @@ void create_aerodromes() {
   };
   initialize(&Provin, "Provin", false);
   initialize(&Proville, "Proville", false);
+  initialize(&Pronville, "Pronville", false);
+  initialize(&Guesnain, "Guesnain", false);
+  initialize(&Phalempin, "Phalempin", false);
+  initialize(&Faumont, "Faumont", false);
+  initialize(&Bersee, "Bersée", false);
+  initialize(&Roucourt, "Roucourt", false);
+  initialize(&Epinoy, "Épinoy", false);
+  initialize(&Villers_au_Tetre, "Villers-au-Tetre", false);
+  initialize(&LaBrayelle, "La Brayelle", false);
+  initialize(&Gonnelieu, "Gonnelieu", false);
+  initialize(&Boistrancourt, "Boistrancourt", false);
+  initialize(&Eswars, "Eswars", false);
+  initialize(&Pont_a_Marcq, "Pont-à-Marcq", false);
+  initialize(&Cuincy, "Cuincy", false);
+  initialize(&Cantin, "Cantin", false);
+  initialize(&Emerchicourt, "Émerchicourt", false);
+  initialize(&Corbehem, "Corbehem", false);
+  initialize(&Abacon, "Abacon", false);
 }
