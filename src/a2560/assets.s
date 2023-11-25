a150a13c023db78d004e466710e1f8f5939593e1
Håkan Thörngren <hth313@gmail.com>
2023-11-25 10:22:12 -0800

    Move A2560 assets source

---
+++ src/a2560/assets.s
@@ -0,0 +1,36 @@
+#define VRAM_OFFSET 0x00c00000
+
+sprite:       .macro sym, sprite_file
+              .align  2
+              .public \sym
+\sym:         .byte   .byte0 (sprite_data$ - VRAM_OFFSET)
+              .byte   1             ; control byte
+              .word   .word1 (sprite_data$ - VRAM_OFFSET)
+              .word   0,0
+              .section vram,data
+              .align  256
+sprite_data$: .incbin "\sprite_file"
+              .section near,data
+              .endm
+
+              sprite  SopwithPup_sprite, "../assets/Pup.raw"
+              sprite  SopwithTriplane_sprite, "../assets/Triplane.raw"
+              sprite  Albatros_D5a_J3_sprite, "../assets/Albatros-D5a-J3.raw"
+              sprite  Albatros_D2_sprite, "../assets/Alatros-D2.raw"
+              sprite  Albatros_D2_sprite_red_baron, "../assets/Albatros-D2-Red-Baron.raw"
+
+              .section palette1,data
+              .public palette
+palette:      .incbin "../assets/BloodyAprilSprites.pal"
+
+// ----------------------------------------------------------------------
+// Tile maps
+
+tiles:        .macro  tiles
+              .section vram,data
+              .public \tiles_index, \tiles_tiles
+\tiles_index: .incbin "../../assets/\tiles.index"
+\tiles_tiles: .incbin "../../assets/\tiles.tiledata"
+
+              tiles   trench1
+              tiles   towns
---
+++ src/a2560/graphics.c
@@ -0,0 +1,16 @@
+#include <foenix/vicky.h>
+
+void initialize() {
+  Vicky.master_control =
+      GRAPHIC_MODE_ENABLE | TILE_ENGINE_ENABLE | SPRITE_ENGINE_ENABLE;
+}
+
+void initialize_tilemap(vram_ptr tile_map[4], vram_ptr tile_graphics[4]) {
+  for (unsigned i = 0; i < 4; i++) {
+    Tilemap[i].data = tile_map[i];
+    Tilemap[i].layer_enable = 1;
+    Tilemap[i].width = 112;
+    Tilemap[i].height = 64;
+    Tileset[i].addy = tile_graphics[i];
+  }
+}
