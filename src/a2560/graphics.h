2d85ccec3afc0a5f7bdc67753ddcdc2e0b45f3ce
Håkan Thörngren <hth313@gmail.com>
2023-11-28 22:18:02 -0800

    Add missing header file

---
+++ src/a2560/graphics.h
@@ -0,0 +1,7 @@
+#ifndef __A2560_GRAPHICS_H__
+#define __A2560_GRAPHICS_H__
+
+extern struct sprite right_facing_dogfight_sprite;
+extern struct sprite left_facing_dogfight_sprite;
+
+#endif // __A2560_GRAPHICS_H__
