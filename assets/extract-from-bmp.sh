7d7f5201931afcf4563308568804c3de1ab64a23
Håkan Thörngren <hth313@gmail.com>
2023-11-30 22:11:08 -0800

    Update asset creation script

--- assets/extract-from-bmp.sh
+++ assets/extract-from-bmp.sh
@@ -1,3 +1,3 @@
-tiler --extract-palette --noise-on-color 0 --noise-color 22 --noise-color 26 --noise-level 6 trench1.bmp
-tiler towns.bmp
-tiler --width=32 --height=32 --sprites --sprite-count=32 sprites.bmp
+tiler --big-endian --tile-lut=1 --tile-set=0 --extract-palette --noise-on-color 0 --noise-color 22 --noise-color 26 --noise-level 6 trench1.bmp
+tiler --big-endian --tile-lut=1 --tile-set=1 towns.bmp
+tiler --big-endian --width=32 --height=32 --sprites --sprite-count=32 sprites.bmp
