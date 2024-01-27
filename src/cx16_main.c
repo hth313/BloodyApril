a8372d2a592cc342a484be6504078be473b2fbc7
Håkan Thörngren <hth313@gmail.com>
2024-01-27 10:27:24 -0800

    Add some CX16 specific stubs

---
+++ src/cx16_main.c
@@ -0,0 +1,11 @@
+#include "random.h"
+
+static unsigned values[20];
+
+int main() {
+  init_seed();
+
+  for (unsigned i = 0; i < 20; i++) {
+    values[i] = roll2d10();
+  }
+}
---
+++ src/cx16_ui.c
@@ -0,0 +1,11 @@
+#include "ui.h"
+
+void cursor_to(enum cursor_position to) {
+  switch (to) {
+  case CommandPosition:
+    break;
+  }
+}
+
+void draw_dogfight(struct dogfight* df) {
+}
